1f5338ab5291510e0d7ff7fdc816e96a7bddcedd
Traun Leyden <traun@couchbase.com>
2013-07-23 17:30:35 -0700

    add ability to add X docs

--- src/main/java/com/couchbase/cblite/cbliteconsole/CBLiteConsoleActivity.java
+++ src/main/java/com/couchbase/cblite/cbliteconsole/CBLiteConsoleActivity.java
@@ -1,12 +1,16 @@
 package com.couchbase.cblite.cbliteconsole;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Editable;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -66,12 +70,30 @@ public class CBLiteConsoleActivity extends Activity {
         final Button buttonAddDoc = (Button) findViewById(R.id.buttonAddDoc);
         buttonAddDoc.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                int docCountBefore = database.getDocumentCount();
-                TestObject test = new TestObject(1, false, "console doc");
-                couchDbConnector.create(test);
-                String message = String.format("Doc added.  Doc count before: %d, after: %d", docCountBefore, database.getDocumentCount());
-                Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG);
-                toast.show();
+                createXTestDocs(1);
+            }
+        });
+
+        final Button buttonAddXDocs = (Button) findViewById(R.id.buttonAddXDocs);
+        final EditText input = new EditText(this);
+        buttonAddXDocs.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new AlertDialog.Builder(CBLiteConsoleActivity.this)
+                        .setTitle("How many docs?")
+                        .setMessage("Enter the number of test docs you want to add")
+                        .setView(input)
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                Editable value = input.getText();
+                                String stringValue = value.toString();
+                                int intValue = Integer.parseInt(stringValue);
+                                createXTestDocs(intValue);
+                            }
+                        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Do nothing.
+                    }
+                }).show();
             }
         });
 
@@ -83,6 +105,28 @@ public class CBLiteConsoleActivity extends Activity {
                 toast.show();
             }
         });
+
+    }
+
+    private void createXTestDocs(final int numberOfDocs) {
+
+        EktorpAsyncTask asyncTask = new EktorpAsyncTask() {
+            @Override
+            protected void doInBackground() {
+                final int docCountBefore = database.getDocumentCount();
+                for (int i=0; i<numberOfDocs; i++) {
+                    TestObject test = new TestObject(1, false, "console doc");
+                    couchDbConnector.create(test);
+                }
+                CBLiteConsoleActivity.this.runOnUiThread(new Runnable() {
+                    public void run() {
+                        String message = String.format("Docs added.  Doc count before: %d, after: %d", docCountBefore, database.getDocumentCount());
+                        Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG);
+                        toast.show();                    }
+                });
+            }
+        };
+        asyncTask.execute();
 
     }
 
